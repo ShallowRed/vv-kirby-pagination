935e842b3326154c97fc82b4292dc9884f981657
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2024-07-23 17:28:43 +0200

    Revert "Add last and first item"
    This reverts commit e654b35d71e974e403d13d870eb27ad26ef0f0d1.

--- snippets/pagination.php
+++ snippets/pagination.php
@@ -1,43 +1,35 @@
-<?php $pagination = $collection->pagination() ?>
-<nav class='collection-pagination'>
-  <ul>
-    <?php if ($pagination->hasPrevPage()) : ?>
-    <li class="collection-pagination__item collection-pagination__item--to-first">
-      <a href="<?php echo $pagination->firstPageURL() ?>">
-        <span class="collection-pagination__icon collection-pagination__icon--first"></span>
-      </a>
-    </li>
-    <li class="collection-pagination__item collection-pagination__item--to-sibling">
-      <a href="<?php echo $pagination->prevPageURL() ?>">
-        <span class="collection-pagination__icon collection-pagination__icon--prev"></span>
-      </a>
-    </li>
-    <?php endif ?>
-
-    <?php foreach ($pagination->range($range ?? 10) as $r) : ?>
-    <li class="collection-pagination__item collection-pagination__item--to-number">
-      <a <?php
-      echo attr([
-        'href' => $pagination->pageURL($r),
-        'aria-current' => $pagination->page() === $r ? 'page' : null,
-      ])
-          ?>>
-        <?php echo $r ?>
-      </a>
-    </li>
-    <?php endforeach ?>
-
-    <?php if ($pagination->hasNextPage()) : ?>
-    <li class="collection-pagination__item collection-pagination__item--to-sibling">
-      <a href="<?php echo $pagination->nextPageURL() ?>">
-        <span class="collection-pagination__icon collection-pagination__icon--next"></span>
-      </a>
-    </li>
-    <li class="collection-pagination__item collection-pagination__item--to-last">
-      <a href="<?php echo $pagination->lastPageURL() ?>">
-        <span class="collection-pagination__icon collection-pagination__icon--last"></span>
-      </a>
-    </li>
-    <?php endif ?>
-  </ul>
+<nav <?php
+echo attr(A::extend([
+    'class' => "pagination"
+], $attrs ?? []));
+?>
+>
+  <div class="pagination__inner">
+    <?php if ($hasPrev ?? false) : ?>
+    <?php
+      snippet('pagination-item', [
+        'label' => $prevLabel,
+        'href' => $prev->url(),
+        'attrs' => [
+          'class' => 'pagination__item pagination__item--prev',
+        ],
+      ]);
+      ?>
+    <?php else : ?>
+    <div class="pagination__item"></div>
+    <?php endif; ?>
+    <?php if ($hasNext ?? false) : ?>
+    <?php
+      snippet('pagination-item', [
+        'label' => $nextLabel,
+        'href' => $next->url(),
+        'attrs' => [
+          'class' => 'pagination__item pagination__item--next',
+        ]
+      ]);
+      ?>
+    <?php else : ?>
+    <div class="pagination__item"></div>
+    <?php endif; ?>
+  </div>
 </nav>
